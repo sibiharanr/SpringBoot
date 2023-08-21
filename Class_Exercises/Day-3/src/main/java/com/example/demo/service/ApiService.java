03cbdc7cf718e701f5f48760b14678a6fe665d2a
Sibiharan <124172962+sibiharanr@users.noreply.github.com>
2023-08-21 15:55:54 +0530

    Add files via upload

---
+++ Class_Exercises/Day-3/src/main/java/com/example/demo/service/ApiService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.ApiModel;
+import com.example.demo.repository.ApiRepo;
+
+@Service
+public class ApiService {
+	
+	@Autowired
+	ApiRepo ar;
+	
+	public ApiModel saveinfo(ApiModel am) {
+		return ar.save(am);
+	}
+	
+	public List<ApiModel> showinfo(){
+		return ar.findAll();
+	}
+}
